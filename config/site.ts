c45eaedcbcb5892fe9126e0ecd58ff0839efe5b0
Connor Bradley <c.bradley.dev@gmail.com>
2023-04-20 14:12:10 +0100

    Added NextAuth, daisyUI part 2

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "BioChef",
+  description:
+    "BioChef is a luxury food delivery service that creates fresh, healthy, and delicious meal plans based on your unique dietary needs and preferences.",
+  url: "https://biochef.vercel.app",
+  ogImage: "https://biochef.vercel.app/og.jpg",
+  links: {
+    github: "https://github.com/connorbradley26/BioChef",
+  },
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#2C3E50",
+          secondary: "#B7850B",
+          accent: "#1FB2A5",
+          neutral: "#808080",
+          "base-100": "#2A303C",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
-}
+};
